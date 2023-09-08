726d19482b22f9de4966285cf1935fde98725921
Aayush Nepal <nepalaayush@outlook.com>
2023-09-08 16:53:51 +0200

    homographic transformation

--- skeletonization.py
+++ skeletonization.py
@@ -10,8 +10,8 @@ import numpy as np
 import napari
 import nibabel as nib 
 import matplotlib.pyplot as plt 
-from scipy.ndimage import gaussian_filter
-from skimage.morphology import skeletonize, binary_erosion
+from scipy.ndimage import gaussian_filter, zoom
+from skimage.morphology import skeletonize, binary_erosion, square, erosion, dilation, remove_small_objects
 #%%
 def open_nii(path):
     ''' Input: Path of nifti file (.nii) Output: pixelarray  ''' 
@@ -82,8 +82,6 @@ def create_low_pass_filter(image_shape, sigma=80):
 
     gaussian_numerator = (X - centerX)**2 + (Y - centerY)**2
 
-
-otsu
     # Create high-pass filter using Gaussian equation
 
     H = np.exp(-gaussian_numerator / (2 * sigma ** 2))
@@ -109,6 +107,28 @@ def bwskel(image):
     import skimage.morphology as sm
 
     return sm.skeletonize(image)
+
+
+def gradify(pixelarray):
+    dy, dx = np.gradient(pixelarray, axis=(1, 2))
+
+    # Magnitude of the gradient for each frame
+    gradient_magnitude = np.sqrt(dy**2 + dx**2)
+    return gradient_magnitude
+
+def apply_filter_to_kspace(image):
+    image_fft = np.fft.fft2(image)
+    viewer.add_image(np.abs(image_fft), name='image_fft')
+
+
+
+    filtered_fft = image_fft * low_pass_filter
+    viewer.add_image(np.abs(filtered_fft), name='filtered_fft') 
+
+
+    filtered_image = np.fft.ifft2(filtered_fft)
+    viewer.add_image(np.abs(filtered_image), name='filtered_image')
+
 #%%
 image = open_nii('/data/projects/ma-nepal-segmentation/data/CINE_HighRes.nii')
 
@@ -116,65 +136,163 @@ image = normalize(image)
 
 #%%
 viewer = napari.view_image(image)
-#%%
-log_transformed_image = log_transform(image)
-
-viewer.add_image(log_transformed_image, name='Log Transformed Image')
 
 #%%
-# Test function with a dummy image shape
+smooth_image = gaussian_filter(image, 2)
+viewer.add_image(smooth_image)
+#%%
 
-image_shape = (256, 256)  # Replace with your actual image shape
+low_pass_filter = create_low_pass_filter(image[0].shape, sigma=40)
 
-high_pass_filter = create_high_pass_filter(image_shape)
-
-actual_high = 1 - high_pass_filter
-
-viewer.add_image(actual_high, name='High-Pass Filter')
-
+viewer.add_image(low_pass_filter, name='low pass filter_40')
 
 #%%
-def apply_high_pass_filter(image, low_pass_filter):
-    # Perform Fourier Transform on the image
-    image_fft = np.fft.fft2(image)
-    #image_fft_shifted = np.fft.fftshift(image_fft)  # FFT shift
-    #viewer.add_image(np.abs(image_fft), name='Non Shifted image fft')
-    # Convert the low-pass filter to a high-pass filter
-    high_pass_filter = 1 - low_pass_filter
-    #viewer.add_image(high_pass_filter, name = 'high pass filter')
-    # Apply the high-pass filter
-    filtered_fft = image_fft * high_pass_filter
+
+grad_smooth = gradify(smooth_image)
+viewer.add_image(grad_smooth, name='gradient_smooth')
+#%%
+apply_filter_to_kspace(grad_smooth)
+#%%
+#import cv2
+#resampled_image = cv2.resize(grad_smooth, grad_smooth.shape, interpolation=cv2.INTER_CUBIC)
+
+#resampled_image = zoom(grad_smooth, zoom=1, order=3) # this doesnt do much. 
+#%%
+#viewer.add_image(resampled_image, name='resampled')
+
+#%%
+binary_image = grad_smooth < 0.8
+
+viewer.add_image(binary_image, name='binary')
+
+#%%
+from skimage import feature 
+
+def apply_canny(pixelarray, low, high):
+    canny_edge = np.zeros_like(grad_smooth)
     
-    # Perform inverse Fourier Transform to get the filtered image back
-    filtered_image = np.fft.ifft2(filtered_fft)
-    
-    return np.abs(filtered_image)
+    for i in range(grad_smooth.shape[0]):
+        canny_image = feature.canny(grad_smooth[i], low_threshold= low, high_threshold=high )    
+        canny_edge[i] = canny_image
+    return canny_edge.astype(dtype=bool)
 
 
+canny_edge = apply_canny(grad_smooth, low=0.1, high=1)
+#%%
+viewer.add_image(canny_edge, name='canny_direct')
+#%%
+from skimage.measure import label
 
-low_pass_filter = create_low_pass_filter(image.shape, sigma=80)
-# Apply the high-pass filter to the image
-filtered_image = apply_high_pass_filter(image, low_pass_filter)
-viewer.add_image(filtered_image, name='Filtered Image')
+labeled_image, num_features = label(canny_edge, return_num=True, connectivity=1)
+viewer.add_image(labeled_image, name='con_1_label')
 
 #%%
-def apply_gaussian_smoothing(image, sigma=20):
-    """
-    Apply Gaussian smoothing to the image.
-    
-    Parameters:
-        image (numpy.ndarray): 2D array representing the image.
-        sigma (float): The standard deviation of the Gaussian kernel.
-        
-    Returns:
-        numpy.ndarray: The smoothed image.
-    """
-    return gaussian_filter(image, sigma)
 
-# Assuming 'filtered_image' is the image obtained after high-pass filtering
-smoothed_image = apply_gaussian_smoothing(filtered_image, sigma=2)
+fem_label = labeled_image == 173 
 
-viewer.add_image(smoothed_image)
+viewer.add_image(fem_label)
+
+#%%
+# extract the coordinates from the fem_label 
+
+from skimage.measure import regionprops
+
+props = regionprops(fem_label.astype(int))
+coords = props[0].coords  # since `fem_label` contains only one label, we take the first item
+
+#%%
+# another way to get coords.. which is potentially better:
+
+z, y, x = np.where(fem_label)
+
+# Stack them into an N x 3 NumPy array
+coords = np.column_stack((z, y, x))    
+#%%
+#to get the coords in a napari shape format:
+num_frames = fem_label.shape[0]
+
+# Initialize an empty list to store the grouped arrays
+grouped_coords = []
+
+# Loop through each frame to group the coordinates
+for frame in range(num_frames):
+    grouped_array = coords[coords[:, 0] == frame]
+    grouped_coords.append(grouped_array)
+#%%
+viewer.add_shapes(grouped_coords, shape_type='polygon')
+#%%
+import cv2
+''' a slightly different stuff of finding the transformation between sets of points.  ''' 
+setA = grouped_coords[0]
+setB = grouped_coords[1]
+
+setA = setA[:,1:]
+setB = setB[:,1:]
+
+
+H, mask = cv2.findHomography(setA, setB, cv2.RANSAC)
+#%%
+homogeneous_A = np.hstack([setA, np.ones((setA.shape[0], 1))])
+
+# Transform the points
+transformed_A_homogeneous = homogeneous_A @ H.T  # Note the transpose of H
+
+# Convert back to 2D coordinates
+transformed_A = transformed_A_homogeneous[:, :2] / transformed_A_homogeneous[:, 2, np.newaxis]
+
+print("Transformed A:")
+print(transformed_A)
+#%%
+
+viewer1 = napari.view_image(grad_smooth[0]) 
+#%%
+viewer1.add_shapes(transformed_A, shape_type='polygon', name='H.dot.A')
+#%%
+# now we tery to do it for when set A and B do NOT have the same number of points 
+
+setB = grouped_coords[20][:,1:]
+#%%
+from sklearn.neighbors import NearestNeighbors
+nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(setA)
+distances, indices = nbrs.kneighbors(setB)
+
+# Extract the corresponding points
+A_corresponding = setA[indices.flatten()]
+#%%
+H, _ = cv2.findHomography(A_corresponding, setB, cv2.RANSAC)
+
+homogeneous_A = np.hstack([setA, np.ones((setA.shape[0], 1))])
+
+# Transform the points
+transformed_A_homogeneous = homogeneous_A @ H.T  # Note the transpose of H
+
+# Convert back to 2D coordinates
+transformed_A = transformed_A_homogeneous[:, :2] / transformed_A_homogeneous[:, 2, np.newaxis]
+#%%
+
+viewer.add_points(transformed_A, name='transformed_A')
+#%%
+dilated_image = dilation(canny_edge[0], square(3))
+eroded_image = erosion(dilated_image, square(3))
+
+#viewer.add_image(eroded_image, name='eroded_image_3')
+#%%
+def apply_remove_small(pixelarray, min_size):
+    removed_small = np.zeros_like(pixelarray)
+    for i in range(pixelarray.shape[0]):
+        removed_small[i] = remove_small_objects(pixelarray[i], min_size)
+    return removed_small
+
+removed_small = apply_remove_small(canny_edge, min_size=5)
+
+viewer.add_image(removed_small, name = 'min_size_5')
+
+#%%
+from skimage import measure, io 
+
+labeled_image, num_features = measure.label(canny_edge, return_num=True, connectivity=1)
+#%%
+viewer.add_image(labeled_image , name='labeled_image_connectivity_1')
 
 #%%
 
@@ -223,8 +341,8 @@ smooth_array = np.zeros_like(image)
 low_pass_filter = create_low_pass_filter(image[0].shape, sigma=80)
 for i in range(image.shape[0]):
     filtered_image = apply_high_pass_filter(image[i], low_pass_filter)
-    smoothed_image = gaussian_filter(image[i], 2)
-    smooth_array[i] = smoothed_image
+    #smoothed_image = gaussian_filter(image[i], 2)
+    smooth_array[i] = filtered_image
     
 #%%
 viewer = napari.view_image(smooth_array)
@@ -232,12 +350,7 @@ viewer = napari.view_image(smooth_array)
 viewer.add_image(smooth_array)
 
 #%%
-def gradify(pixelarray):
-    dy, dx = np.gradient(pixelarray, axis=(1, 2))
 
-    # Magnitude of the gradient for each frame
-    gradient_magnitude = np.sqrt(dy**2 + dx**2)
-    return gradient_magnitude
 
 gradient_smooth = gradify(smooth_array)
 
