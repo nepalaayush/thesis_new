6c20f780f9b91b655e5b06a2da38de4d4241478d
unknown <nepalaayush@outlook.com>
2024-07-02 18:57:39 +0200

    updating till ds3

--- manual_seg.py
+++ manual_seg.py
@@ -16,21 +16,24 @@ import time
 from scipy import ndimage
 import matplotlib.pylab as plt 
 
-
+import pandas as pd 
 
 from utils import (path_to_image, apply_canny, apply_remove, apply_skeleton, points_for_napari,
                    boolean_to_coords, apply_label, find_tibia_edges, find_array_with_min_n, downsample_points,
                    combined_consecutive_transform, coords_to_boolean, process_frame, sample_points_in_polygon,
                    fit_pca_line, get_uv_from_pca, find_edges_nnew, find_intersection, show_stuff)
 
-    
+
+#%%
+with open('C:/Users/Aayush/Documents/thesis_files/thesis_new/ds1_angles_nw.pkl', 'rb') as file:
+    ds1_angles_nw = pickle.load(file)        
 #%%
 # Step 1: load the image from directory and normalize it
-path_neg = 'C:/Users/Aayush/Documents/thesis_files/manual_segmentation_datasets/MM_2_dataset1/MM_NW_ai2_tgv_5e-2_neg_ngn.nii'
-path_pos ='C:/Users/Aayush/Documents/thesis_files/manual_segmentation_datasets/MM_2_dataset1/MM_NW_ai2_tgv_5e-2_pos_ngn.nii'
+path_neg = 'C:/Users/Aayush/Documents/thesis_files/manual_segmentation_datasets/AN_5_dataset3/AN_NW_ai2__5e-2_neg.nii'
+path_pos ='C:/Users/Aayush/Documents/thesis_files/manual_segmentation_datasets/AN_5_dataset3/AN_NW_ai2_5e-2_pos_ngn.nii'
 #%%
-image_neg = path_to_image(path_neg)[2:]
-image_pos = path_to_image(path_pos)[2:]
+image_neg = path_to_image(path_neg)[::-1]
+image_pos = path_to_image(path_pos)[::-1] 
 #%%
 # since our image goes from extened to flexed.. the direction means, pos is going down.. and neg is coming up 
 # which means. if we want to present our data as going up then coming down .. we have to reverse the neg, put it at the first half. 
@@ -40,7 +43,7 @@ full_image = np.concatenate( (image_neg, image_pos) , axis=0)
 
 #%%
 #add the original image to napari
-viewer = napari.view_image(full_image,  name='ds1_NW_full')
+viewer = napari.view_image(full_image,  name='ds3_NW_full')
 
 #%%
 
@@ -183,10 +186,47 @@ def process_multi_frame(shape_layer, image_shape):
     
     return frame_dict
 
-tib_info = process_multi_frame(shapes_data, full_image.shape)
+tib_info = process_multi_frame(viewer.layers['tib'], full_image.shape)
 
-fem_info = process_multi_frame(viewer.layers['femur'], full_image.shape)
+fem_info = process_multi_frame(viewer.layers['fem'], full_image.shape)
 
 #%%
-ds1_angles_nw = calculate_and_plot_angles_between_bones(tib_info, fem_info, name='ds_1_NW')
+ds3_angles_nw = calculate_and_plot_angles_between_bones(tib_info, fem_info, name='ds_3_NW')
 
+#%%
+with open('tib_info_ds3.pkl', 'wb') as f:
+    pickle.dump(tib_info, f)   
+
+with open('fem_info_ds3.pkl', 'wb') as f:
+    pickle.dump(fem_info, f)   
+
+with open('ds3_angles_nw.pkl', 'wb') as f:
+    pickle.dump(ds3_angles_nw, f)   
+
+
+#%%
+# create dataframe from the angle array.. but the angles have not been modified, and neither have the 
+def create_angle_dataframe(angle_array, dataset_id):
+    """
+    Create a dataframe from an angle array with specified columns.
+    
+    :param angle_array: Array of angle values.
+    :param dataset_id: Identifier for the dataset.
+    :return: Dataframe with columns ['Frame', 'Dataset', 'angle', 'Condition'].
+    """
+    # Create a dataframe
+    df = pd.DataFrame({
+        'Frame': range(len(angle_array)),  # Frame numbers as index
+        'Dataset': dataset_id,             # Dataset identifier
+        'angle': angle_array,              # Angle values
+        'Condition': 'Manual'              # Condition set to 'Manual'
+    })
+    
+    return df
+
+
+ds3_angle_df = create_angle_dataframe(ds3_angles_nw, 3)
+
+#%%
+with open('ds3_angles_df.pkl', 'wb') as f:
+    pickle.dump(ds3_angle_df, f)   
